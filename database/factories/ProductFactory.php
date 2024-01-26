92b172e95adb901e37ac346f0c445fdd8e37e005
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-01-26 20:42:56 +0200

    added product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'price' => fake()->numberBetween(0, 2000),
+            'description' => fake()->numberBetween(0, 2000),
+            'image' => fake()->imageUrl(640, 480, 'animals', true),
+
+
+
+
+
+        ];
+    }
+}
