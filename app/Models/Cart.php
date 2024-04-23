928734956449be2971636c010f329dc0f2b76cb9
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-04-23 22:12:07 +0300

    test

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -11,6 +11,6 @@ class Cart extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'userZ_id');
+        return $this->belongsTo(User::class, 'users_id');
     }
 }
