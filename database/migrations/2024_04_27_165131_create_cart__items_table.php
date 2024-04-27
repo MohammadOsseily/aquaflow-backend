dcb9b07c90fe9f121c4ec180ea3f2a4ed477a050
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-04-27 21:16:18 +0300

    added cart list added relations

--- database/migrations/2024_04_27_165131_create_cart__items_table.php
+++ database/migrations/2024_04_27_165131_create_cart__items_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Cart;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,13 +12,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        if (!Schema::hasTable('carts')) {
-            Schema::create('carts', function (Blueprint $table) {
-                $table->id();
-                $table->foreignId("user_id")->constrained();
-                $table->timestamps();
-            });
-        }
+        Schema::create('cart__items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("cart_id")->constrained();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('carts');
+        Schema::dropIfExists('cart__items');
     }
 };
